f53463f6a5bda457ea1f4a9e94e98ad3201ce81c
jmf991 <jmf991@hotmail.com>
2016-05-10 15:58:47 +0200

    no message

---
+++ ADOproject/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace ADOproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datagridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_fName = new System.Windows.Forms.TextBox();
+            this.textBox_lName = new System.Windows.Forms.TextBox();
+            this.textBox_phone = new System.Windows.Forms.TextBox();
+            this.textBox_nombre = new System.Windows.Forms.Label();
+            this.textBox_apellidos = new System.Windows.Forms.Label();
+            this.textBox_telefono = new System.Windows.Forms.Label();
+            this.button_cargarListado = new System.Windows.Forms.Button();
+            this.button_insert = new System.Windows.Forms.Button();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.button_update = new System.Windows.Forms.Button();
+            this.textBox_IdPhoneBook = new System.Windows.Forms.TextBox();
+            this.textBox_InternalId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_indice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_filas = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // datagridView1
+            // 
+            this.datagridView1.AllowDrop = true;
+            this.datagridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagridView1.Location = new System.Drawing.Point(27, 33);
+            this.datagridView1.Name = "datagridView1";
+            this.datagridView1.Size = new System.Drawing.Size(642, 300);
+            this.datagridView1.TabIndex = 0;
+            // 
+            // textBox_fName
+            // 
+            this.textBox_fName.Location = new System.Drawing.Point(105, 386);
+            this.textBox_fName.Name = "textBox_fName";
+            this.textBox_fName.Size = new System.Drawing.Size(294, 20);
+            this.textBox_fName.TabIndex = 4;
+            // 
+            // textBox_lName
+            // 
+            this.textBox_lName.Location = new System.Drawing.Point(105, 417);
+            this.textBox_lName.Name = "textBox_lName";
+            this.textBox_lName.Size = new System.Drawing.Size(294, 20);
+            this.textBox_lName.TabIndex = 5;
+            // 
+            // textBox_phone
+            // 
+            this.textBox_phone.Location = new System.Drawing.Point(105, 444);
+            this.textBox_phone.Name = "textBox_phone";
+            this.textBox_phone.Size = new System.Drawing.Size(294, 20);
+            this.textBox_phone.TabIndex = 6;
+            // 
+            // textBox_nombre
+            // 
+            this.textBox_nombre.AutoSize = true;
+            this.textBox_nombre.Location = new System.Drawing.Point(24, 386);
+            this.textBox_nombre.Name = "textBox_nombre";
+            this.textBox_nombre.Size = new System.Drawing.Size(38, 13);
+            this.textBox_nombre.TabIndex = 7;
+            this.textBox_nombre.Text = "fName";
+            // 
+            // textBox_apellidos
+            // 
+            this.textBox_apellidos.AutoSize = true;
+            this.textBox_apellidos.Location = new System.Drawing.Point(24, 417);
+            this.textBox_apellidos.Name = "textBox_apellidos";
+            this.textBox_apellidos.Size = new System.Drawing.Size(37, 13);
+            this.textBox_apellidos.TabIndex = 8;
+            this.textBox_apellidos.Text = "lName";
+            // 
+            // textBox_telefono
+            // 
+            this.textBox_telefono.AutoSize = true;
+            this.textBox_telefono.Location = new System.Drawing.Point(24, 444);
+            this.textBox_telefono.Name = "textBox_telefono";
+            this.textBox_telefono.Size = new System.Drawing.Size(64, 13);
+            this.textBox_telefono.TabIndex = 9;
+            this.textBox_telefono.Text = "Nº Teléfono";
+            // 
+            // button_cargarListado
+            // 
+            this.button_cargarListado.Location = new System.Drawing.Point(696, 33);
+            this.button_cargarListado.Name = "button_cargarListado";
+            this.button_cargarListado.Size = new System.Drawing.Size(137, 69);
+            this.button_cargarListado.TabIndex = 10;
+            this.button_cargarListado.Text = "Cargar Listado Telefónico";
+            this.button_cargarListado.UseVisualStyleBackColor = true;
+            this.button_cargarListado.Click += new System.EventHandler(this.button_cargarListado_Click);
+            // 
+            // button_insert
+            // 
+            this.button_insert.Location = new System.Drawing.Point(696, 139);
+            this.button_insert.Name = "button_insert";
+            this.button_insert.Size = new System.Drawing.Size(75, 23);
+            this.button_insert.TabIndex = 11;
+            this.button_insert.Text = "Añadir";
+            this.button_insert.UseVisualStyleBackColor = true;
+            this.button_insert.Click += new System.EventHandler(this.button_insert_Click);
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(696, 169);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(75, 23);
+            this.button_delete.TabIndex = 12;
+            this.button_delete.Text = "Borrar";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // button_update
+            // 
+            this.button_update.Location = new System.Drawing.Point(696, 199);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(75, 23);
+            this.button_update.TabIndex = 13;
+            this.button_update.Text = "Update";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // textBox_IdPhoneBook
+            // 
+            this.textBox_IdPhoneBook.Location = new System.Drawing.Point(105, 470);
+            this.textBox_IdPhoneBook.Name = "textBox_IdPhoneBook";
+            this.textBox_IdPhoneBook.Size = new System.Drawing.Size(193, 20);
+            this.textBox_IdPhoneBook.TabIndex = 14;
+            // 
+            // textBox_InternalId
+            // 
+            this.textBox_InternalId.Location = new System.Drawing.Point(105, 496);
+            this.textBox_InternalId.Name = "textBox_InternalId";
+            this.textBox_InternalId.Size = new System.Drawing.Size(193, 20);
+            this.textBox_InternalId.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 473);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "IdPhoneBook";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 496);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "InternalId";
+            // 
+            // textBox_indice
+            // 
+            this.textBox_indice.Location = new System.Drawing.Point(45, 560);
+            this.textBox_indice.Name = "textBox_indice";
+            this.textBox_indice.Size = new System.Drawing.Size(100, 20);
+            this.textBox_indice.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 544);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Indice de la fila seleccionada";
+            // 
+            // textBox_filas
+            // 
+            this.textBox_filas.Location = new System.Drawing.Point(210, 560);
+            this.textBox_filas.Name = "textBox_filas";
+            this.textBox_filas.Size = new System.Drawing.Size(100, 20);
+            this.textBox_filas.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(225, 544);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Nº Filas llenas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 673);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_filas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_indice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_InternalId);
+            this.Controls.Add(this.textBox_IdPhoneBook);
+            this.Controls.Add(this.button_update);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.button_insert);
+            this.Controls.Add(this.button_cargarListado);
+            this.Controls.Add(this.textBox_telefono);
+            this.Controls.Add(this.textBox_apellidos);
+            this.Controls.Add(this.textBox_nombre);
+            this.Controls.Add(this.textBox_phone);
+            this.Controls.Add(this.textBox_lName);
+            this.Controls.Add(this.textBox_fName);
+            this.Controls.Add(this.datagridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.datagridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView datagridView1;
+        private System.Windows.Forms.TextBox textBox_fName;
+        private System.Windows.Forms.TextBox textBox_lName;
+        private System.Windows.Forms.TextBox textBox_phone;
+        private System.Windows.Forms.Label textBox_nombre;
+        private System.Windows.Forms.Label textBox_apellidos;
+        private System.Windows.Forms.Label textBox_telefono;
+        private System.Windows.Forms.Button button_cargarListado;
+        private System.Windows.Forms.Button button_insert;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.TextBox textBox_IdPhoneBook;
+        private System.Windows.Forms.TextBox textBox_InternalId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_indice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_filas;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ADOproject/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+using System.Diagnostics;//para Debug.Assert
+
+namespace ADOproject
+{
+    public partial class Form1 : Form
+    {
+        string datosConexion = "Data Source=net-2;Initial Catalog=listaTelefonos;Integrated Security=true;";//cadena conexion
+        SqlConnection conn = new SqlConnection();//conexion
+        SqlDataAdapter dataadapter = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //CARGAR LISTADO
+        private void button_cargarListado_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = datosConexion;
+                conn.Open();//abre conexion
+
+                string sql = "SELECT * FROM telefonosTable4"; //selecciona todos los datos de telefonosTable4
+                SqlConnection connection = new SqlConnection(datosConexion);//inicializa los datos de la conexion
+                SqlDataAdapter dataadapter = new SqlDataAdapter(sql, connection);//inicializa el adapter de la conexion
+                DataSet ds = new DataSet();//inicializa el dataset 
+                //Si no se incluye este dataset aqui, se mostrara un nuevo ds (sucesivamente) cada vez que se clique el boton
+
+                dataadapter.Fill(ds, "phoneBookList_table");//rellena el dataset 
+                datagridView1.AllowUserToAddRows = false;//eliminamos la ultima fila del datagridview que se autoagrega  
+                connection.Close();//cierra conexion
+                datagridView1.DataSource = ds;//aplica al datagridView el dataset como su dataSource
+                datagridView1.DataMember = "phoneBookList_table";
+
+                rowsCounter(); //metodo para contar el total de rows mostradas
+                hider(); //metodo para ocultar las filas borradas (con valor 0 en la columna de estaActivo)
+            }
+            catch (MySql.Data.MySqlClient.MySqlException)
+            {
+                MessageBox.Show("Error de Carga");
+                return;
+            }
+        }
+        // END CARGAR LISTADO
+
+        //INSERTAR ROW EN DB
+        private void button_insert_Click(object sender, EventArgs e)
+        {   // se especifican los campos y valores a insertar
+            string cmdString = "INSERT INTO telefonosTable4 (fName,lName,phone, IdPhoneBook, InternalId, estaActivo) VALUES (@val1, @val2, @val3, @val4, @val5, @val6)";
+            using (SqlConnection conn = new SqlConnection(datosConexion))
+            {
+                using (SqlCommand comm = new SqlCommand())
+                {
+                    comm.Connection = conn;
+                    comm.CommandText = cmdString;
+                    comm.Parameters.AddWithValue("@val1", textBox_fName.Text);//valor a insertar para fName
+                    comm.Parameters.AddWithValue("@val2", textBox_lName.Text);//valor a insertar para lName
+                    comm.Parameters.AddWithValue("@val3", textBox_phone.Text);//valor a insertar para phone
+                    comm.Parameters.AddWithValue("@val4", textBox_IdPhoneBook.Text);//valor a insertar para IdPhoneBook
+                    comm.Parameters.AddWithValue("@val5", textBox_InternalId.Text);//valor a insertar para InternalId
+                    comm.Parameters.AddWithValue("@val6", 1);//valor a insertar para estaActivo (Por defecto 1, para que se muestre)
+
+                    try
+                    {
+                        conn.Open();//se abre conexion
+                        comm.ExecuteNonQuery();//se ejecuta comando
+                        MessageBox.Show("datos insertados en la base de datos");
+                        conn.Close();//se cierra conexion
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("Error: es posible que los datos no estén en el formato correcto");
+                        return;
+                    }
+                }
+            }
+        }
+        // END INSERTAR ROW EN DB
+
+        //BORRAR ROW EN DB
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            if (datagridView1.SelectedRows.Count > 0) //se debe haber seleccionado al menos una fila a borrar, si no saltará un error
+            {
+                int selectedRow = this.datagridView1.SelectedRows[0].Index; //Para localizar la fila a borrar
+
+                if (this.datagridView1.Rows[selectedRow].Cells["InternalId"].Value != null) //Si el valor del del InternalId no es null
+                {
+                    System.Text.StringBuilder InternalId = new System.Text.StringBuilder(); //Muestra valor de InternalId de la fila seleccionada
+
+                    InternalId.Append(datagridView1.Rows[selectedRow].Cells["InternalId"].Value.ToString());
+                    MessageBox.Show("El InternalId de la fila a borrar es: " + InternalId);
+
+                    //Si la fila está vacia el valor del InternalId será incorrecto (espacio en blanco)
+                    int i = 0;
+                    for (i = 0; i >= InternalId.Length; ) //si el la columa de InternalId esta vacia:
+                    {
+                        MessageBox.Show("No se puede borrar ya que la fila seleccionada está vacía, o el falta el valor de la columna InternalId");
+                        return;
+                    }
+
+                    //donde el valor del InternalId coincida con el de la fila seleccionada se cambiará el valor de estaActivo
+                    string cmdString = "UPDATE telefonosTable4 SET estaActivo = @activo WHERE InternalId =" + InternalId;
+
+                    using (SqlConnection conn = new SqlConnection(datosConexion))
+                    {
+                        using (SqlCommand comm = new SqlCommand())
+                        {
+                            comm.Connection = conn;
+                            comm.CommandText = cmdString;
+                            comm.Parameters.AddWithValue("@activo", 0); //Cambia el valor de estaActivo
+
+                            try
+                            {
+                                conn.Open();//se abre conexion
+                                comm.ExecuteNonQuery();//se ejecuta el comando
+                                MessageBox.Show("El valor de la columna estaActivo ha cambiado a 0,\n pulsa cargar para ver los cambios");
+                                conn.Close();//se cierra conexion
+                                //datagridView1.Rows.RemoveAt(this.datagridView1.SelectedRows[0].Index);
+                            }
+                            catch (SqlException)
+                            {
+                                MessageBox.Show("Error de Borrado");
+                            }
+                            if (this.datagridView1.Rows[selectedRow].Cells["estaActivo"].Value != null)
+                            {
+                                if (this.datagridView1.Rows[selectedRow].Cells["estaActivo"].Value is DBNull)
+                                {
+                                    MessageBox.Show("El valor de la columna estaActivo es null");
+                                    return;
+                                }
+                                littleHider();
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Hay que seleccionar una fila");
+            }
+        }
+        // END BORRAR ROW EN DB
+
+        // OCULTADOR DE FILAS GENERAL
+        private void hider()// Oculta todas las filas con valor 0 en la columna estaActivo 
+        {
+            foreach (DataGridViewRow row in datagridView1.Rows)
+            {
+                if (row.Cells["estaActivo"].Value == DBNull.Value)//Si estaActivo es null:
+                {
+                    MessageBox.Show("Una de las filas tiene la columna estaActivo null, se mostrará vacía");
+
+                    datagridView1.CurrentCell = null;
+                    this.datagridView1.Columns["estaActivo"].Visible = true;
+
+                    continue;
+                }
+                if (Convert.ToInt32(row.Cells["estaActivo"].Value) != 1)//Si estaActivo es distinto de 1:
+                {
+                    datagridView1.CurrentCell = null;//Se nulliza la celda para poder ocultarla
+                    row.Visible = false;//se oculta
+                }
+            }
+        }
+        // END OCULTADOR DE FILAS GENERAL
+
+        // OCULTADOR DE FILAS
+        private void littleHider() // Oculta la fila seleccionada con valor 0 en la columna estaActivo 
+        {
+            int selectedRow = this.datagridView1.SelectedRows[0].Index;
+
+            if (this.datagridView1.Rows[selectedRow].Cells["estaActivo"].Value is DBNull)//Si estaActivo es null:
+            {
+                MessageBox.Show("la columna estaActivo es null, Por lo que no se puede borrar");
+                return;
+            }
+            if (Convert.ToInt32(this.datagridView1.Rows[selectedRow].Cells["estaActivo"].Value) != 1)//Si estaActivo es distinto de 1:
+            {
+                datagridView1.CurrentCell = null; //Se nulliza la celda para poder ocultarla
+                datagridView1.Rows[selectedRow].Visible = false;//se oculta
+                datagridView1.Rows.RemoveAt(this.datagridView1.SelectedRows[0].Index);//Se borra del datagridview
+            }
+        }
+        // END OCULTADOR DE FILAS
+
+        //UPDATE
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(datosConexion))
+            {
+                using (SqlCommand comm = new SqlCommand())
+                {
+                    comm.Connection = conn;
+                    comm.CommandText = "UPDATE telefonosTable4 SET updated = @updated";
+                    comm.Parameters.AddWithValue("@updated", "sí"); //Cambia el valor de update a "Si"
+
+                    try
+                    {
+                        conn.Open();//Abre conexion
+                        comm.ExecuteNonQuery();//Ejecuta comando
+                        MessageBox.Show("Se ha hecho Update con todos los registros");
+                        conn.Close();//Cierra conexion
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("Error de Update");
+                    }
+                }
+            }
+        }
+        // END UPDATE
+
+        // CONTADOR DE FILAS
+        private void rowsCounter()
+        {
+            var count = datagridView1.Rows.Cast<DataGridViewRow>().Count(row => row.Cells["estaActivo"].Value != System.DBNull.Value);
+            this.textBox_filas.Text = count.ToString();
+            //MessageBox.Show("Numero de filas: " + count);
+        }
+        // END CONTADOR DE FILAS
+
+        // CONTADOR DE INDICE
+        //private void GetRowIndex()
+        //{
+        //    //Debug.Assert(this.datagridView1.CurrentCell.RowIndex == null, "Debes seleccionar la fila que quieres borrar");           
+        //        int rowIndex = this.datagridView1.CurrentCell.RowIndex + 1;
+        //        this.textBox_indice.Text = rowIndex.ToString();
+        //        //MessageBox.Show("Indice de la fila seleccionada: " + rowIndex);            
+        //}
+        // END CONTADOR DE INDICE
+
+        // CERRAR LA APLICACION
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                if (MessageBox.Show("¿Quiere salir de la Lista Telefónica?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+            base.OnFormClosing(e);
+        }
+        // END CERRAR LA APLICACION
+    }
+}
+
+
